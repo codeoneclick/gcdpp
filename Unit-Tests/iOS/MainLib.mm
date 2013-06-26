d26d2d13d5aa14cb77c02b48808c130855761bba
Sergey Sergeev <sergey.sergeev@khamac0039.kha.gameloft.org>
2013-06-26 18:04:55 +0300

    added TEST for std::packaged_task

--- Unit-Tests/iOS/MainLib.mm
+++ Unit-Tests/iOS/MainLib.mm
@@ -12,6 +12,18 @@
 
 @implementation MainLib
 
+
+std::queue<std::packaged_task<void()>> queue;
+
+template<class FUNCTION, class ...ARGS>
+std::future<typename std::result_of<FUNCTION(ARGS...)>::type>
+dispatch(FUNCTION &&f, ARGS &&...args) {
+    std::packaged_task<typename std::result_of<FUNCTION(ARGS...)>::type()> task(std::bind(f,args...));
+    auto future = task.get_future();
+    queue.push(std::packaged_task<void()>(std::move(task)));
+    return future;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     std::thread::id this_id = std::this_thread::get_id();
@@ -29,6 +41,15 @@
     NSTimer *sender = [NSTimer timerWithTimeInterval:0.1 invocation:invocation repeats:YES];
     NSRunLoop *runner = [NSRunLoop currentRunLoop];
     [runner addTimer:sender forMode:NSDefaultRunLoopMode];
+    
+    std::function<void(int, float, std::string)> function = [](int a, float b, const std::string& c)
+    {
+        EXPECT_TRUE(a == 1 && b == 2.0f && c == "Hello World");
+    };
+    
+    auto future = dispatch(std::move(function), 1, 2.0f, "Hello World");
+    
+    queue.back()();
     
     std::cout<<"[Testing] "<<std::endl<<RUN_ALL_TESTS()<<std::endl;
     
